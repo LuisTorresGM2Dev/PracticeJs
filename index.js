7f82b9091fdb1cf96cb77635d8a16f150b7602d9
GM2 Academy <gm2academy@MacBook-Pro-de-Tecnico.local>
2023-08-07 12:24:06 -0300

    Comienzo

---
+++ index.js
@@ -0,0 +1,10 @@
+function Producto(Nombre, Precio, descuento, precioDescuento, cuotas, descripcion){
+    return 
+    `Nombre: $(ombre)
+            Precio: $(precio)
+            Descuento: $(descuento)
+            Precio con descuento: $(precioDescuento)
+            Hasta 12 cuotas sin interes: $(cuotas)
+            Descripcion: $(descripcion)
+            ` 
+   }
